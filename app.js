f530cbca99607b118fa05ea8f843b2b664ba43a2
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-01-26 17:59:51 +0800

    additional improvements

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-const generate = document.getElementById('generate')
-const currentColours = document.getElementById('currentColours')
-const mainPanel = document.getElementById('mainPanel')
-const storePanel = document.getElementById('storePanel')
+const generate = document.getElementById('generate');
+const currentColours = document.getElementById('currentColours');
+const mainPanel = document.getElementById('mainPanel');
+const storePanel = document.getElementById('storePanel');
 
-const repeat = document.getElementById('repeat')
+const RGB = document.getElementById('RGB');
 
-const redIncrease = document.getElementById('redIncrease')
-const greenIncrease = document.getElementById('greenIncrease')
-const blueIncrease = document.getElementById('blueIncrease')
+const repeat = document.getElementById('repeat');
 
-// const RGB = document.getElementById('RGB')
+const redIncrease = document.getElementById('redIncrease');
+const greenIncrease = document.getElementById('greenIncrease');
+const blueIncrease = document.getElementById('blueIncrease');
 
-const red = document.getElementById('red');
-const green = document.getElementById('green');
-const blue = document.getElementById('blue');
+let hex
+let obj = new Object()
 
-let hex 
+if (storePanel.innerHTML.trim().length == 0) {
+  storePanel.style.visibility = 'hidden'
+} 
 
-const rgbToObj = rgb => {
-  const colors = ["red", "green", "blue", "alpha"]
+function rgbToObj(rgb) {
+  let colors = ["red", "green", "blue", "alpha"]
   let colorArr = rgb.slice(
       rgb.indexOf("(") + 1, 
       rgb.indexOf(")")
-  ).split(", ");
-  let obj = new Object();
+  ).split(/, | ,|,/);
+
   colorArr.forEach((k, i) => {
-      obj[colors[i]] = k
+      obj[colors[i]] = Number(k)
   })
   return obj;
 }
 
-const rgbToHex = (r, g, b) => {
-  hex = "#" + ((1 << 24) + (Number(r) << 16) + (Number(g) << 8) + Number(b)).toString(16).slice(1);
+const rgbToHex = (obj) => {
+  hex = "#" + ((1 << 24) + (obj.red << 16) + (obj.green << 8) + obj.blue).toString(16).slice(1);
   return
 }
 
 const reset = () => {
-  red.value = 0, green.value = 0, blue.value = 0;
   while(currentColours.firstChild || storePanel.firstChild) {
     currentColours.innerHTML = ""  
     storePanel.innerHTML = ""  
+    storePanel.style.visibility = 'hidden'
   }
 }
 
@@ -47,6 +48,7 @@ const store = () => {
   if (!currentColours.innerHTML.trim().length == 0) {
     const newContainer = document.createElement('div') 
     newContainer.classList.add('storedColours')
+    storePanel.style.visibility = 'visible'
     storePanel.appendChild(newContainer)
     while (currentColours.childNodes.length > 0) {
       newContainer.appendChild(currentColours.childNodes[0]);
@@ -59,29 +61,37 @@ const store = () => {
 }
 
 generate.addEventListener('click', function() {
-  let i = 0
+  currentColours.innerHTML = ""  
+  if (RGB.value === "") {
+    iziToast.error({
+      title: 'Insert starting RGB colour',
+    });  
+    return
+  }
+  
+  rgbToObj(RGB.value)
+  
   if (repeat.value < 1) {
     iziToast.error({
       title: 'Pick a number of the repeats',
     });  
   }
-
+  
+  let i = 0
   while(i < repeat.value * 1) {
-    red.value = Number(red.value) + (Number(blueIncrease.value))
-    green.value = Number(green.value) + (Number(greenIncrease.value))
-    blue.value = Number(blue.value) + (Number(blueIncrease.value))
-      
-    if (blue.value > 255 || green.value > 255 ||red.value > 255) {
-      iziToast.error({
-      title: 'Going beyond the limits',
-      message: 'Values must stay below 255'
-      });
-    break
-    }
+    obj = {'red': obj.red + Number(redIncrease.value), 'green': obj.green + Number(greenIncrease.value), 'blue': obj.blue + Number(blueIncrease.value)}
     
-    rgbToHex(red.value, green.value, blue.value)
-    const rgb = `rgb(${red.value}, ${green.value}, ${blue.value})`
-
+    // if (obj.blue > 255 || obj.green > 255 ||obj.red > 255) {
+      //   iziToast.error({
+        //   title: 'Going beyond the limits',
+        //   message: 'Values must stay below 255'
+        //   });
+    // break
+    // }
+    
+    rgbToHex(obj)
+    const rgb = `rgb(${obj.red}, ${obj.green}, ${obj.blue})`
+    
     const colourCard = document.createElement('div'); 
     colourCard.classList.add('colourCard')
     currentColours.appendChild(colourCard); 
@@ -90,6 +100,3 @@ generate.addEventListener('click', function() {
     i++
   }
 });
-
-// TODO
-// - convert input to RGB object
