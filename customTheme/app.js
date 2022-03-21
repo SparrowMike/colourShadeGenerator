285a10ff23c5f3ae581602ab259209f4c64849bf
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-03-21 17:08:41 +0800

    fix: 🐛 selected theme border not showing

--- customTheme/app.js
+++ customTheme/app.js
@@ -286,9 +286,7 @@ app.controller("theme", function ($scope, $http) {
   //? ==============Recieve Message==============
   window.onmessage = function(e) {
     const data = e.data;
-    if (e.origin === 'https://mock-up-three.vercel.app/') return;
-    $('.defaultTheme').removeClass('active')
-    $(`.${data.selectedTheme}`).addClass('active')
+    if (['https://mock-up-three.vercel.app/', 'http://127.0.0.1:5501'].includes(e.origin)) return;
     if (!['', null, undefined, 'null', {}].includes(JSON.stringify(data.currentPalette))) {
       localStorage.setItem("customTheme", JSON.stringify(data.currentPalette));  
       localStorageCustomTheme = data.currentPalette;
